50b3e300db625e687f86d0cb960ae9b468992a78
Roberto Broy <roberto.broy@powerschool.com>
2019-11-04 18:48:41 -0800

    add TestSt.java

--- Student.java
+++ Student.java
@@ -1,5 +1,9 @@
-class Student {
-	Student() {
-		this.name = "John";
-	}
+public class Student {
+	String name;
+    Student() {
+        this.name = "John";
+    }
+    public void printStudent() {
+        System.out.println("Name: " + this.name);
+    }
 }
---
+++ TestSt.java
@@ -0,0 +1,7 @@
+
+public class TestSt {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.printStudent();
+    }
+}
